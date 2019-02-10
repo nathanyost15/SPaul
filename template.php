546895c059b72ca4939870e5bc56e20f76a52485
nathan yost <nathanyost15@gmail.com>
2019-02-10 15:23:36 -0500

    Added template.php page, and cleaned up directory with some organization.

--- template.php
+++ template.php
@@ -1,18 +1,16 @@
 <html>
     <head>
-        <title>Wood 5</title>
+        <title>SPaul</title>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="static/styles/style.css">
     </head>
     <body>
         <?php
+            // Inserts navbar to page.
             require_once("static/html/navbar.html");
         ?>
-        <div class="container">
-            <img id="welcomeimage" src="img/Welcome Page(cropped).jpg">
-        </div>
     </body>
 </html>
